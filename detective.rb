afbc405e1db9fc0d04643248bdbf4ce8e8da9c86
Yulin Chen <gloverock2@gmail.com>
2016-03-28 08:28:12 +0800

    Change the implementation of Detective.

--- detective.rb
+++ detective.rb
@@ -1,5 +1,18 @@
+# 改變 Detective 的實作
+# 讓它跟別的物件有互動
+# 讓情況稍微複雜一點點
+# 比較貼近真實情況
+# 也才會面臨需要考慮使用
+# stub, mock 或 spy 等技巧的狀況
+#
+# 初始化代入一個參數
+# 這個物件要可以回應 prod 方法
 class Detective
+  def initialize(thingie)
+    @thingie = thingie
+  end
+
   def investigate
-    "Nothing to investigate :'("
+    "It went #{@thingie.prod}"
   end
 end
--- spec/detective_spec.rb
+++ spec/detective_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../detective'
 
+# 為了使這個測試通過，需要改變什麼呢？
+# Detective 初始化時代入一個可以回應 prod 的物件？
+# 改變預期的回傳字串嗎？
 RSpec.describe Detective do
   it "doesn't find much" do
     subject = Detective.new
